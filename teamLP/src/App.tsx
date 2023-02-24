a8d530f8a1c1dd69fac61c5ec977eb3f2bda03f3
Rikuto <orikutobasuke@gmail.com>
2023-02-24 09:59:32 +0900

    teamLP add

--- teamLP/src/App.tsx
+++ teamLP/src/App.tsx
@@ -4,360 +4,218 @@ import './style.css'
 
 function App() {
   return(
-    <Box
-        bg={"#F5F3E4"}
-        overflow={"hidden"}
-    >
-      <Flex
-        justifyContent={"flex-end"}
-        position={"relative"}
-      >
-        <Box
-          as='img'
-          src='./public/img/グループ 5373.png'
-          alt='右画面'
-          position={"absolute"}
-          top={"265px"}
-          right={"-120px"}
-          w={"600px"}
-          h={"400px"}
-          zIndex={"1"}
-          ></Box>
-          <Box
-            as='img'
-            src='./public/img/グループ 5357.png'
-            alt='右の木'
-            w={"400px"}
-            h={"500px"}
-            position={"absolute"}
-            top={"80px"}
-            right={"130px"}
-            zIndex={"0"}
-          ></Box>
-          <Box
-            as='img'
-            src='./public/img/グループ 5375.png'
-            alt='fun'
-            w={"400px"}
-            h={"200px"}
-            position={"absolute"}
-            top={"70px"}
-            right={"30px"}
-          ></Box>
+    <Box bg={"#F5F3E4"}overflow={"hidden"}>
+      <Flex justifyContent={"flex-end"}position={"relative"}>
+        <Box as='img' src='./public/img/グループ 5373.png' alt='右画面' position={"absolute"} top={"265px"} right={"-120px"} w={"600px"} h={"400px"} zIndex={"1"}></Box>
+        <Box as='img' src='./public/img/グループ 5357.png' alt='右の木' w={"400px"} h={"500px"} position={"absolute"} top={"80px"} right={"130px"} zIndex={"0"}></Box>
+        <Box as='img'src='./public/img/グループ 5375.png'alt='fun'w={"400px"}h={"200px"}position={"absolute"}top={"70px"}right={"30px"}></Box>
       </Flex>
-      <Box
-        as='img'
-        src='./public/img/グループ 5377.png'
-        w={"450px"}
-        h={"150px"}
-        position={"absolute"}
-        top={"40px"}
-        left={"30px"}
-        zIndex={"1"}
-      ></Box>
-      <Box
-        as='img'
-        src='./public/img/グループ 5363.png'
-        alt='左の木'
-        w={"400px"}
-        h={"500px"}
-        position={"absolute"}
-        left={"140px"}
-        zIndex={"0"}
-      ></Box>
-      <Box
-        as='img'
-        src='./img/グループ 5359.png'
-        alt='左画面'
-        position={"absolute"}
-        top={"265px"}
-        left={"-120px"}
-        w={"600px"}
-        h={"400px"}
-      ></Box>
+      <Box as='img'src='./public/img/グループ 5377.png'w={"450px"}h={"150px"}position={"absolute"}top={"40px"}left={"30px"}zIndex={"1"}></Box>
+      <Box as='img'src='./public/img/グループ 5363.png'alt='左の木'w={"400px"}h={"500px"}position={"absolute"}left={"140px"}zIndex={"0"}></Box>
+      <Box as='img'src='./img/グループ 5359.png'alt='左画面'position={"absolute"}top={"265px"}left={"-120px"}w={"600px"}h={"400px"}></Box>
       <Center>
-        <Box 
-          as='img' 
-          src='./img/logo.png' 
-          alt='logo' 
-          w={"600px"} 
-          h={"180px"}
-          marginTop={"70px"}
-          zIndex={"9999"}
-        ></Box>
+        <Box as='img' src='./img/logo.png' alt='logo' w={"600px"} h={"180px"}marginTop={"70px"}zIndex={"9999"}></Box>
       </Center>
       <Center>
-        <Box 
-          as='img' 
-          src='./img/アプリを通じて子どもの家のお手伝いをサポートするアプリお手伝いをして自分だけの動物園を作ろう！.png' 
-          alt='文字' 
-          w={"500px"} 
-          h={"130px"}
-          marginTop={"10px"}
-        ></Box>
+        <Box as='img' src='./img/アプリを通じて子どもの家のお手伝いをサポートするアプリお手伝いをして自分だけの動物園を作ろう！.png' alt='文字' w={"500px"} h={"130px"}marginTop={"10px"}></Box>
       </Center>
       <Center>
-        <Box
-          as='img'
-          src='./img/グループ 5361.png'
-          w={"450px"} 
-          h={"60px"}
-          marginTop={"20px"}
-        ></Box>
+        <Box as='img' src='./img/グループ 5361.png'w={"450px"} h={"60px"}marginTop={"20px"}></Box>
       </Center>
       <Center position={"relative"}>
-        <Box
-          as='img'
-          src='./public/img/グループ 5362.png'
-          alt='うさぎ'
-          position={"absolute"}
-          top={"30px"}
-          left={"420px"}
-        ></Box>
-        <Box
-          as='img'
-          src='./public/img/グループ 5364.png'
-          alt='らいおん'
-          position={"absolute"}
-          top={"50px"}
-          right={"460px"}
-          zIndex={"1"}
-        ></Box>
+        <Box as='img'src='./public/img/グループ 5362.png'alt='うさぎ'position={"absolute"}top={"30px"}left={"420px"}></Box>
+        <Box as='img'src='./public/img/グループ 5364.png'alt='らいおん'position={"absolute"}top={"50px"}right={"460px"}zIndex={"1"}></Box>
       </Center>
-      <Center 
-          w={"100%"}
-          >
-        <Box 
-          as='img' 
-          src='./public/img/グループ 5372.png' 
-          alt='reef'
-          maxW={"none"}
-          w={"200%"}
-          display={"block"}
-          ></Box>
+      <Center w={"100%"}>
+        <Box as='img' src='./public/img/グループ 5372.png' alt='reef'maxW={"none"}w={"200%"}display={"block"}></Box>
       </Center>
       <Center position={"relative"}>
-        <Box
-          as='img'
-          src='./public/img/グループ 5381.png'
-          w={"1200px"}
-          h={"200px"}
-          marginTop={"50px"}
-        >
-        </Box>
-          <h2 className='h2'>子供のお手伝いをサポートするサービス</h2>
+        <Box as='img'src='./public/img/グループ 5381.png'w={"1200px"}h={"200px"}marginTop={"50px"}></Box>
       </Center>
-      <Box 
-          w={"100%"}
-          h={"100%"}
+      <Center>
+        <Box as='img'src='./public/img/子供のお手伝いをサポートするサービス.png'w={"700px"}></Box>
+      </Center>
+
+
+      <Box w={"100%"}h={"100%"}>
+
+
+        <Box position={"relative"}w={"100%"}h={"1000px"}>
+          <Box as='img'src='./public/img/グループ 5382.png'position={"absolute"}top={"100px"}zIndex={"0"}></Box>
+          <Flex
+            justifyContent={"space-between"}
+            position={"relative"}
           >
-        <Box 
-          position={"relative"}
-          w={"100%"}
-          h={"1000px"}
-        >
-          <Box
-            as='img'
-            src='./public/img/グループ 5382.png'
-            position={"absolute"}
-            top={"100px"}
-            zIndex={"0"}
-            ></Box>
-          <Flex 
-              justifyContent={"center"}
-              position={"relative"}
-              >
             <Box
               as='img'
-              src='./public/img/グループ 5393.png'
-              w={"600px"}
-              h={"400px"}
-              position={"absolute"}
-              top={"200px"}
-              left={"130px"}
+              src='./public/img/グループ 5422.png'
+              w={"250px"}
+              h={"150px"}
               zIndex={"1"}
+              position={"absolute"}
+              top={"90px"}
             ></Box>
             <Box
               as='img'
-              src='./public/img/グループ 5392.png'
-              w={"600px"}
-              h={"400px"}
-              position={"absolute"}
-              top={"300px"}
-              right={"130px"}
+              src='./public/img/グループ 5423.png'
+              w={"200px"}
+              h={"130px"}
               zIndex={"1"}
+              position={"absolute"}
+              top={"180px"}
+              right={"40px"}
             ></Box>
-            {/* <Box></Box> */}
           </Flex>
-        </Box>
-        <Box
-          position={"relative"}
-          w={"100%"}
-          h={"1000px"}
-        >
-          <Box
-            as='img'
-            src='./public/img/グループ 5384.png'
-            position={"absolute"}
-            top={"25px"}
-            ></Box>
-            <Flex 
-              justifyContent={"center"}
-              position={"relative"}
-              >
+          <Flex justifyContent={"center"}position={"relative"}>
+            <Box as='img'src='./public/img/グループ 5393.png'w={"600px"}h={"400px"}position={"absolute"}top={"200px"}left={"130px"}zIndex={"1"}></Box>
+            <Box as='img'src='./public/img/グループ 5392.png'w={"600px"}h={"400px"}position={"absolute"}top={"300px"}right={"130px"}zIndex={"1"}></Box>
+          </Flex>
+          <Center position={"relative"}>
+            <Box as='img'src='./public/img/グループ 5555.png'w={"700px"}position={"absolute"}top={"750px"}></Box>
+          </Center>
+          <Flex
+            justifyContent={"space-between"}
+            position={"relative"}
+          >
             <Box
               as='img'
-              src='./public/img/グループ 5413.png'
-              w={"600px"}
-              h={"400px"}
-              position={"absolute"}
-              top={"140px"}
-              left={"130px"}
+              src='./public/img/グループ 5432.png'
+              w={"200px"}
+              h={"150px"}
               zIndex={"1"}
+              position={"absolute"}
+              top={"800px"}
             ></Box>
             <Box
               as='img'
-              src='./public/img/グループ 5411.png'
-              w={"600px"}
-              h={"400px"}
-              position={"absolute"}
-              top={"240px"}
-              right={"130px"}
+              src='./public/img/グループ 5431.png'
+              w={"180px"}
+              h={"150px"}
               zIndex={"1"}
+              position={"absolute"}
+              top={"800px"}
+              right={"40px"}
             ></Box>
           </Flex>
         </Box>
-        <Box
-          position={"relative"}
-          w={"100%"}
-          h={"1000px"}
-        >
-          <Box
-            as='img'
-            src='./public/img/グループ 5387.png'
-            position={"absolute"}
-            bottom={"40px"}
-            ></Box>
-              <Flex 
-              justifyContent={"center"}
-              position={"relative"}
-              >
+
+
+        <Box position={"relative"}w={"100%"}h={"1000px"}>
+          <Box as='img'src='./public/img/グループ 5384.png'position={"absolute"}top={"25px"}></Box>
+          <Flex
+            justifyContent={"space-between"}
+            position={"relative"}
+          >
             <Box
               as='img'
-              src='./public/img/グループ 5408.png'
-              w={"600px"}
-              h={"400px"}
-              position={"absolute"}
-              top={"50px"}
-              left={"130px"}
+              src='./public/img/グループ 5435.png'
+              w={"200px"}
+              h={"150px"}
               zIndex={"1"}
+              position={"absolute"}
+              top={"800px"}
             ></Box>
             <Box
               as='img'
-              src='./public/img/グループ 5409.png'
-              w={"600px"}
-              h={"400px"}
-              position={"absolute"}
-              top={"150px"}
-              right={"130px"}
+              src='./public/img/マスクグループ 4806.png'
+              w={"180px"}
+              h={"150px"}
               zIndex={"1"}
+              position={"absolute"}
+              top={"800px"}
+              right={"40px"}
             ></Box>
           </Flex>
+            <Flex justifyContent={"center"}position={"relative"}>
+              <Box as='img'src='./public/img/グループ 5413.png'w={"600px"}h={"400px"}position={"absolute"}top={"140px"}left={"130px"}zIndex={"1"}></Box>
+              <Box as='img'src='./public/img/グループ 5411.png'w={"600px"}h={"400px"}position={"absolute"}top={"240px"}right={"130px"}zIndex={"1"}></Box>
+            </Flex>
+            <Center position={"relative"}>
+              <Box as='img'src='./public/img/グループ 5429.png'w={"700px"}position={"absolute"}top={"700px"}zIndex={"1"}></Box>
+            </Center>
         </Box>
-        <Center
-          marginTop={"60px"}
-          marginBottom={"200px"}
-        >
-          <Box
-            as='img'
-            src='./public/img/グループ 5421.png'
+
+
+        <Box position={"relative"}w={"100%"}h={"1000px"}>
+          <Box as='img'src='./public/img/グループ 5387.png'position={"absolute"}bottom={"40px"}></Box>
+          <Flex
+            justifyContent={"space-between"}
+            position={"relative"}
+          >
+            <Box
+              as='img'
+              src='./public/img/マスクグループ 4808.png'
+              w={"200px"}
+              h={"150px"}
+              zIndex={"1"}
+              position={"absolute"}
+              top={"700px"}
             ></Box>
+            <Box
+              as='img'
+              src='./public/img/マスクグループ 4807.png'
+              w={"180px"}
+              h={"200px"}
+              zIndex={"1"}
+              position={"absolute"}
+              top={"700px"}
+              right={"40px"}
+            ></Box>
+          </Flex>
+            <Flex justifyContent={"center"}position={"relative"}>
+              <Box as='img'src='./public/img/グループ 5408.png'w={"600px"}h={"400px"}position={"absolute"}top={"50px"}left={"130px"}zIndex={"1"}></Box>
+              <Box as='img'src='./public/img/グループ 5409.png'w={"600px"}h={"400px"}position={"absolute"}top={"150px"}right={"130px"}zIndex={"1"}></Box>
+            </Flex>
+            <Center position={"relative"}>
+              <Box as='img'src='./public/img/グループ 5430.png'w={"700px"}position={"absolute"}top={"600px"}zIndex={"1"}></Box>
+            </Center>
+        </Box>
+
+      <Box>
+        <Center marginTop={"60px"}marginBottom={"200px"}>
+          <Box as='img'src='./public/img/グループ 5421.png'></Box>
         </Center>
-        <Center
-          marginBottom={"80px"}
-        >
-          <Box
-            as='img'
-            src='./public/img/グループ 5415.png'
-            w={"80%"}
-          ></Box>
+        <Center marginBottom={"80px"}>
+          <Box as='img'src='./public/img/グループ 5415.png'w={"80%"}></Box>
         </Center>
-        <Center
-          marginBottom={"80px"}
-        >
-          <Box
-            as='img'
-            src='./public/img/グループ 5420.png'
-            w={"80%"}
-          ></Box>
+        <Center marginBottom={"80px"}>
+          <Box as='img'src='./public/img/グループ 5420.png'w={"80%"}></Box>
         </Center>
-        <Center
-          // marginBottom={"300px"}
-        >
-          <Box
-            as='img'
-            src='./public/img/グループ 5419.png'
-            w={"80%"}
-          ></Box>
+        <Center>
+          <Box as='img'src='./public/img/グループ 5419.png'w={"80%"}></Box>
         </Center>
       </Box>
+
+    </Box>
+
+
       <Box>
-          <Flex
-            justifyContent={"center"}
-            position={"relative"}
-          >
-            <Box
-              as='img'
-              src='./public/img/グループ 5427.png'
-              w={"300px"}
-              h={"350px"}
-              position={"absolute"}
-              top={"220px"}
-              left={"100px"}
-              zIndex={"1"}
-            ></Box>
-            <Box
-              as='img'
-              src='./public/img/グループ 5425.png'
-              w={"400px"}
-              h={"400px"}
-              position={"absolute"}
-              top={"210px"}
-              right={"10px"}
-              zIndex={"1"}
-            ></Box>
-          </Flex>
+        <Flex justifyContent={"center"}position={"relative"}>
+          <Box as='img'src='./public/img/グループ 5427.png'w={"300px"}h={"350px"}position={"absolute"}top={"220px"}left={"100px"}zIndex={"1"}></Box>
+          <Box as='img'src='./public/img/グループ 5425.png'w={"400px"}h={"400px"}position={"absolute"}top={"210px"}right={"10px"}zIndex={"1"}></Box>
+        </Flex>
+        <Center position={"relative"}marginBottom={"100px"}>
+          <Box as='img'src='./public/img/グループ 5426.png'w={"400px"}position={"absolute"}top={"300px"}></Box>
+        </Center>
+        <Center position={"relative"}>
+          <Box as='img'src='./public/img/グループ 5027.png'w={"700px"}position={"absolute"}top={"400px"}></Box>
+        </Center>
         <Center
           position={"relative"}
-          marginBottom={"100px"}
-          >
+        >
           <Box
-          as='img'
-          src='./public/img/グループ 5426.png'
-          w={"400px"}
-          position={"absolute"}
-          top={"300px"}
-          ></Box>
-          </Center>
-          <Center
-            position={"relative"}
-          >
-          <Box
-          as='img'
-          src='./public/img/グループ 5428.png'
-          w={"700px"}
-          position={"absolute"}
-          top={"400px"}
+            as='img'
+            src='./public/img/グループ 5666.png'
+            w={"1400px"}
+            position={"absolute"}
+            top={"900px"}
           ></Box>
         </Center>
         <Center>
-          <Box
-            as='img'
-            src='./public/img/グループ 5371.png'
-            h={"1000px"}
-            ></Box>
+          <Box as='img'src='./public/img/グループ 5440.png'h={"1000px"}></Box>
         </Center>
       </Box>
+
     </Box>
   )
 }
